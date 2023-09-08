5fff716c6c3b616ef26fc85a24e0270a06b47cea
trefania <tarirotrefania@gmail.com>
2023-09-08 17:03:50 +0200

    first task

--- Frontend Tranining/script.js
+++ Frontend Tranining/script.js
@@ -1,20 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const slackUserName = document.querySelector('[data-testid="slackUserName"]');
     const slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
     const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
 
-    // Get current date and time
-    const now = new Date();
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDay = daysOfWeek[now.getUTCDay()];
-    const currentHour = now.getUTCHours();
-    const currentMinute = now.getUTCMinutes();
+    // Get current UTC time in milliseconds
+    const currentUTCTimeMillis = new Date().getTime();
 
-    // Update elements with data
-    slackUserName.textContent = "Your Slack Name"; // Replace with your Slack name
-    slackDisplayImage.src = "slack-profile-image.jpg"; // Replace with your Slack profile image URL
-    slackDisplayImage.alt = "Your Slack Username"; // Replace with your Slack username
-    currentDayOfTheWeek.textContent = `Current Day: ${currentDay}`;
-    currentUTCTime.textContent = `Current UTC Time: ${currentHour}:${currentMinute}`;
+    // Update elements with the current UTC time in milliseconds
+    slackUserName.textContent = "Trefania Tariro Vhareta";
+    slackDisplayImage.src = "tref-slack.png";
+    slackDisplayImage.alt = "Trefania";
+    currentDayOfTheWeek.textContent = `Current Day: Fri 08 September`;
+    currentUTCTime.textContent = `Current UTC Time (ms): ${currentUTCTimeMillis}`;
 });
