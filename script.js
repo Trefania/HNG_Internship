a1c7e994e26c3512ade7c6f48a446f9d16d05d68
trefania <tarirotrefania@gmail.com>
2023-09-08 22:08:15 +0200

    updated the UTC time in milliseconds

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
 
-    // Get current UTC time in milliseconds
-    const currentUTCTimeMillis = new Date().getTime();
+    function updateUTCTime() {
+        const currentUTCTimeMillis = new Date().getTime();
+        currentUTCTime.textContent = `Current UTC Time (ms): ${currentUTCTimeMillis}`;
+    }
 
-    // Update elements with the current UTC time in milliseconds
+    updateUTCTime();
+
+    setInterval(updateUTCTime, 1000);
+
     slackUserName.textContent = "Trefania Tariro Vhareta";
     slackDisplayImage.src = "tref-slack.png";
     slackDisplayImage.alt = "Trefania";
     currentDayOfTheWeek.textContent = `Current Day: Fri 08 September`;
-    currentUTCTime.textContent = `Current UTC Time (ms): ${currentUTCTimeMillis}`;
 });
