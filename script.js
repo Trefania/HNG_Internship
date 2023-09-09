18ec02d4c5f1a3e37829a6c2e7a21b24251c54fe
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-09-09 23:18:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Select elements by data-testid attributes
-    const slackUserName = document.querySelector('[data-testid="slackUserName"]');
-    const slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
-    const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+            // Select elements by data-testid attributes
+            const slackUserName = document.querySelector('[data-testid="slackUserName"]');
+            const slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
+            const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+            const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
 
-    // Function to update UTC time in milliseconds
-    function updateUTCTime() {
-        const currentUTCTimeMillis = new Date().getTime();
-        currentUTCTime.textContent = `Current UTC Time (ms): ${currentUTCTimeMillis}`;
-    }
+            function updateUTCTime() {
+                const currentUTCTimeMillis = new Date().getTime();
+                currentUTCTime.textContent = `Current UTC Time (ms): ${currentUTCTimeMillis}`;
+            }
+            function updateDayOfTheWeek() {
+                const currentDate = new Date();
+                const options = { weekday: 'short', day: '2-digit', month: 'short' };
+                const formattedDate = currentDate.toLocaleDateString('en-US', options);
+                currentDayOfTheWeek.textContent = `Current Date: ${formattedDate.toUpperCase()}`;
+            }
 
-    // Function to update the current day of the week with the date
-    function updateDayOfTheWeek() {
-        const currentDate = new Date();
-        const options = { weekday: 'short', day: '2-digit', month: 'short' };
-        const formattedDate = currentDate.toLocaleDateString('en-US', options);
-        currentDayOfTheWeek.textContent = `Current Date: ${formattedDate.toUpperCase()}`;
-    }
+            updateUTCTime();
+            updateDayOfTheWeek();
 
-    // Initial calls to update UTC time and the day of the week
-    updateUTCTime();
-    updateDayOfTheWeek();
+            setInterval(updateUTCTime, 1000);
 
-    // Update UTC time in milliseconds every second (1000ms)
-    setInterval(updateUTCTime, 1000);
+            setInterval(updateDayOfTheWeek, 86400000);
 
-    // Update the day of the week every day (86400000ms = 24 hours)
-    setInterval(updateDayOfTheWeek, 86400000); // Update once a day
-
-    slackUserName.textContent = "Trefania Tariro Vhareta";
-    slackDisplayImage.src = "https://ca.slack-edge.com/T05FFAA91JP-U05R92LJ44D-gd6d4d9ff738-48";
-    slackDisplayImage.alt = "Trefania";
-});
+            slackUserName.textContent = "Trefania Tariro Vhareta";
+            slackDisplayImage.src = "https://ca.slack-edge.com/T05FFAA91JP-U05R92LJ44D-gd6d4d9ff738-48";
+            slackDisplayImage.alt = "Trefania";
+        });
