40d11906d4f1055f45b200e56a26587b32ad2e07
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-09-09 18:56:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
         const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         const currentDayIndex = new Date().getDay();
         const currentDay = daysOfWeek[currentDayIndex];
-        currentDayOfTheWeek.textContent = `Current Day: ${currentDay}`;
+
+        const currentDate = new Date();
+        const day = currentDate.getDate().toString().padStart(2, '0');
+        const month = currentDate.toLocaleString('en-US', { month: 'long' });
+        const year = currentDate.getFullYear();
+
+        currentDayOfTheWeek.textContent = `Today is ${currentDay}, ${day} ${month} ${year}`;
     }
 
     updateUTCTime();
@@ -22,9 +28,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     setInterval(updateUTCTime, 1000);
 
-    setInterval(updateDayOfTheWeek, 86400000); 
+    setInterval(updateDayOfTheWeek, 86400000);
 
- 
     slackUserName.textContent = "Trefania Tariro Vhareta";
     slackDisplayImage.src = "tref-slack.png";
     slackDisplayImage.alt = "Trefania";
