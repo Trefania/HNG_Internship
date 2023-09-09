b68caec41a73ea707ca57fc73eaacf420da12daa
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-09-09 22:53:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Select elements by data-testid attributes
     const slackUserName = document.querySelector('[data-testid="slackUserName"]');
     const slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
     const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
         currentUTCTime.textContent = `Current UTC Time (ms): ${currentUTCTimeMillis}`;
     }
 
+    // Function to update the current day of the week with the date
     function updateDayOfTheWeek() {
         const currentDate = new Date();
         const options = { weekday: 'short', day: '2-digit', month: 'short' };
@@ -17,15 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
         currentDayOfTheWeek.textContent = `Current Date: ${formattedDate.toUpperCase()}`;
     }
 
+    // Initial calls to update UTC time and the day of the week
     updateUTCTime();
     updateDayOfTheWeek();
 
+    // Update UTC time in milliseconds every second (1000ms)
     setInterval(updateUTCTime, 1000);
 
-    setInterval(updateDayOfTheWeek, 86400000); 
+    // Update the day of the week every day (86400000ms = 24 hours)
+    setInterval(updateDayOfTheWeek, 86400000); // Update once a day
 
     slackUserName.textContent = "Trefania Tariro Vhareta";
-    slackDisplayImage.src = "tref-slack.png";
+    slackDisplayImage.src = "https://ca.slack-edge.com/T05FFAA91JP-U05R92LJ44D-gd6d4d9ff738-48";
     slackDisplayImage.alt = "Trefania";
 });
-
